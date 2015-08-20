e3ef82374aa599f7b39e27dd985bcffc3d37599f
Julio Vinachi <julio899@gmail.com>
2015-08-20 09:49:20 -0430

    carga inicial

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # migracionesDBF
+## Reuerimientos y dependencias
+Importante terner encuenta:
+* servidor Apache funcionando con php5 o superior
+* sudo `apt-get install php-pear php5-dev`
+* haber agregado a las librerias de php dbase.so
+* `sudo pecl install dbase`
+* crear el archivo /etc/php5/apache2/conf.d/dbase.ini
+	e incluir `extension=dbase.so`
+* reiniciar el servidor `sudo service apache2 restart`
---
+++ dbf.php
@@ -0,0 +1,133 @@
+<?php
+// Ruta al archivo dbase
+/*
+$ruta_db = "./DBFWEB/01INV.DBF";
+
+$obj=new DBF($ruta_db);
+$obj->crear_tabla();
+$obj->get_recorrido();
+*/
+
+// Ruta al archivo dbase
+$directorio_de_archivos = "./DBFWEB/";
+$archivos_dbf=array('01INV.DBF','01_INV.DBF','02_CHE.DBF','02_CPC.DBF');
+
+	foreach ($archivos_dbf as $key => $archivo) {
+		$ruta=$directorio_de_archivos.$archivo;
+
+			$obj=new DBF($ruta);
+			$obj->crear_tabla();
+			$obj->get_recorrido();
+	}
+
+
+
+class DBF 
+{
+	var $url;
+	var $campos;
+	var $dbf;
+	var $info_columna;
+	var $tabla_sql;
+	var $tabla_sql_data;
+	var $nombre_tabla;
+	var $campos_array;
+	
+	function __construct($url_dbf)
+	{
+		$this->url=$url_dbf;
+		// Abrir un el archivo dbase
+		$this->dbf = dbase_open($this->url, 0)
+		  or die("¡Error! No se pudo abrir el archivo de base de datos dbase '$this->url'.");
+		
+		// Inicializo valores
+		$this->info_columna = dbase_get_header_info($this->dbf);
+		$this->set_campos();
+		$this->set_nombre_tabla();
+		$this->set_campos_array();
+	}
+	
+	function set_campos(){
+			foreach ($this->info_columna as $key => $value) {
+				$this->campos [] = array(	'name' => $value['name'], 
+											'type' => $value['type'], 
+											'long' => $value['length']
+											) ;
+				}//fin de Foreach
+	}//cierre de Funcion set_campos
+
+	function set_nombre_tabla(){
+		# si estamos en ambiente Linux el delimitador de directorio sera / 
+		$temp=explode('/', $this->url);
+		$this->nombre_tabla=$temp[count($temp)-1];
+		# separamos el nombre del archivo de la extension del mismo
+		$temp2=explode('.', $this->nombre_tabla);
+		# escojo el nombre del archivo que esta en la primera posicion 0
+		$this->nombre_tabla=$temp2[0];
+	}// fin de set_nombre_tabla
+
+	function crear_tabla(){
+		$this->tabla_sql="DROP TABLE IF EXISTS `".$this->nombre_tabla."`;\nCREATE TABLE IF NOT EXISTS `".$this->nombre_tabla."` (\n";
+		foreach ($this->campos as $key => $value) {
+			$this->tabla_sql.="\t`".$value['name']."` ";
+			if($value['type']==='number'){ $this->tabla_sql.="double "; }else{$this->tabla_sql.="char(".$value['long'].") ";}
+			$this->tabla_sql.="DEFAULT NULL";
+			if(	($key+1) < count($this->campos)	){ $this->tabla_sql.=",\n"; }
+
+		}
+		$this->tabla_sql.="\n) DEFAULT CHARSET=utf8;";
+		echo "<pre>".$this->tabla_sql."</pre>";
+	}
+
+	function get_recorrido(){
+			echo "<pre>";
+			$this->get_encabezado_llenado();
+		  	echo $this->tabla_sql_data;
+			echo "</pre>";
+	}
+
+	function get_encabezado_llenado(){
+		if ($this->dbf) {
+		  $numero_registros = dbase_numrecords($this->dbf);
+		      $this->tabla_sql_data="INSERT INTO `".$this->nombre_tabla."` (\n".$this->get_campos_str();
+
+		  for ($i = 1; $i <= $numero_registros; $i++) {
+		      $fila = dbase_get_record_with_names($this->dbf, $i);
+		      $control=0; $this->tabla_sql_data.="(";
+		      	foreach ($fila as $key => $value) {
+		      		if($control < (count($fila)-1) ){
+		      			if($this->campos[$control]['type']==='number'){ $this->tabla_sql_data.=$fila[$key];}else{ $this->tabla_sql_data.="'".$fila[$key]."'"; }
+		      			if($control != (count($fila)-2) ){$this->tabla_sql_data.=",";}else{$this->tabla_sql_data.="),";}
+		      		}
+		      		
+		      		$control++;		      				
+		      	}
+		  }
+		  	$cad = $this->tabla_sql_data;
+			$this->tabla_sql_data = substr ($cad, 0, -1);
+			$this->tabla_sql_data .=";"; 
+		}//Fin IF Principal
+	}
+
+	function get_campos_str(){
+		$temporal="";
+
+		      		foreach ($this->campos as $key => $value) {
+		      					$temporal.="`".$this->campos[$key]['name']."`";
+		      					if( $key+1 < count($this->campos) ){ $temporal.= ",";}else{ $temporal.="\n) VALUES \n";}
+		      				
+		      		}
+		return $temporal;
+	}
+
+	function set_campos_array(){
+		
+		      		foreach ($this->campos as $key => $value) {
+		      					$this->campos_array[]=$this->campos[$key]['name'];
+		      		}
+		
+	}
+
+} # Fin de Clase
+
+?>
